d81b2d5074fe81b5b31f7e91d0a3cb576515caa7
Eric Nyirimana <ericnyirimana31@gmail.com>
2019-06-21 13:59:08 +0200

    Added questions

--- src/services/fakeData.js
+++ src/services/fakeData.js
@@ -6,21 +6,21 @@ const questions = [
 			'Which of the following retains the information it’s storing when the power to the system is turned off?',
 		answers: [ 'CPU', 'ROM', 'RAM', 'DIMM' ],
 		rightAnswer: 'ROM',
-		marks: 2
+		marks: 1
 	},
 	{
 		id: 2,
 		title: 'A network of computers and other devices that is confined to a relatively small space is called?',
 		answers: [ 'Wide Area Network', 'Local Area Network', 'Global Network', 'Peer-to-Peer Network' ],
 		rightAnswer: 'Local Area Network',
-		marks: 3
+		marks: 1
 	},
 	{
 		id: 3,
 		title: 'Every computer connected to the Internet is identified by a unique four-part string, known as',
 		answers: [ 'IP address', 'Host name', 'Domain name', 'None of the above' ],
 		rightAnswer: 'IP address',
-		marks: 3
+		marks: 1
 	},
 	{
 		id: 4,
@@ -32,21 +32,21 @@ const questions = [
 			'Electronic Numerical Integrated Automatic Computer'
 		],
 		rightAnswer: 'Electronic Numerical Integrator and Computer',
-		marks: 2
+		marks: 1
 	},
 	{
 		id: 5,
 		title: 'Which of the following statement is correct?',
 		answers: [ '1 KB = 1024 bytes', '1 MB=2048 bytes', '1 MB = 1000 kilobytes', '1 KB = 1000 bytes' ],
 		rightAnswer: '1 KB = 1024 bytes',
-		marks: 6
+		marks: 1
 	},
 	{
 		id: 6,
 		title: 'Which of the following memories must be refreshed many times per second?',
 		answers: [ 'Static RAM', 'Dynamic RAM', 'EPROM', 'ROM' ],
 		rightAnswer: 'Dynamic RAM',
-		marks: 4
+		marks: 1
 	},
 	{
 		id: 7,
@@ -54,7 +54,31 @@ const questions = [
 			' ______ is a technology that allows telephone calls to be made over computer networks like the Internet',
 		answers: [ 'VoIP', 'GSM', 'Modem', 'CDMA' ],
 		rightAnswer: 'VoIP',
-		marks: 5
+		marks: 1
+	},
+	{
+		id: 8,
+		title:
+			'.PNG refers to',
+		answers: [ 'Image file', 'Movie/animation file', 'Audio file', 'MS Office document' ],
+		rightAnswer: 'Image file',
+		marks: 1
+	},
+	{
+		id: 9,
+		title:
+			'USB is a device used to store data and it stands for',
+		answers: [ 'Unlimited Service Band', 'Unlimited Serial Bus', 'Universal Serial Bus', 'Universal Service Bus' ],
+		rightAnswer: 'Universal Serial Bus',
+		marks: 1
+	},
+	{
+		id: 10,
+		title:
+			'To identify TCP/IP errors such as connection problems ______ command can be used',
+		answers: [ 'FTP', 'Ping', 'POP3', 'STP' ],
+		rightAnswer: 'Ping',
+		marks: 1
 	}
 ];
 export function getQns() {
